8aec81b427d45e43fcf85297a8eef8374625849e
Dennis Lin <lin.dennis18@gmail.com>
2019-02-09 11:50:26 -0800

    Make API call to retrieve meme images

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -3,7 +3,23 @@ import React, { Component } from "react";
 class MemeGenerator extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      topText: "",
+      bottomText: "",
+      randomImg: "http://i.imgflip.com/1bij.jpg",
+      allMemeImgs: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://api.imgflip.com/get_memes")
+      .then(response => response.json())
+      .then(response => {
+        const { memes } = response.data;
+        console.log(memes[0]);
+        this.setState({
+          allMemeImgs: memes
+        });
+      });
   }
   render() {
     return <h1>MemeGenerator</h1>;
