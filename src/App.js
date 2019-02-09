208deac1e6eddfa6a7aec0b42ec2d6298b0bac8f
Dennis Lin <lin.dennis18@gmail.com>
2019-02-09 11:40:45 -0800

    Complete header component and style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import Header from "./Header";
 import MemeGenerator from "./MemeGenerator";
 
+import "./App.css";
+
 function App() {
   return (
     <div>
--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Header() {
-  return <h1>Header</h1>;
+  return (
+    <header>
+      <img
+        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
+        alt="Problem?"
+      />
+      <p>Meme Generator</p>
+    </header>
+  );
 }
 
 export default Header;
