21d5390d6d7b14032f4069d3149662fc2c73af32
Dennis Lin <lin.dennis18@gmail.com>
2019-02-09 11:28:23 -0800

    First commit

---
+++ src/MemeGenerator.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MemeGenerator extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return <h1>MemeGenerator</h1>;
+  }
+}
+
+export default MemeGenerator;
